fa13f4207cfe156749e4c75da4e9150d1de09f6b
Normand Martin <normandmartin@macbook-pro-de-normand-martin.local>
2015-11-08 16:51:22 -0500

    Initial Commit

---
+++ VerbesItalien3Tests/VerbesItalien3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  VerbesItalien3Tests.swift
+//  VerbesItalien3Tests
+//
+//  Created by Normand Martin on 15-11-08.
+//  Copyright © 2015 Normand Martin. All rights reserved.
+//
+
+import XCTest
+@testable import VerbesItalien3
+
+class VerbesItalien3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
